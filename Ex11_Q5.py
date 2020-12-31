160dd545f391bf3022144574c59abf7473552dc3
oriel <orielmalihi1@gmail.com>
2020-12-31 18:35:31 +0200

    all is working as expected

--- Ex11_Q5.py
+++ Ex11_Q5.py
@@ -22,8 +22,20 @@ def find_match(set1: List[Agent], set2: List[Agent]) -> dict:
     >>> d2 = Agent("Batya", ["Shlomo", "Rafi", "Tomer"])
     >>> d3 = Agent("Aviva", ["Rafi", "Shlomo", "Tomer"])
     >>> res = find_match([s1, s2, s3], [d1, d2, d3])
-    >>> print(res)
+    >>> res
     {'Batya': 'Shlomo', 'Aviva': 'Rafi', 'Gila': 'Tomer'}
+    >>> s1 = Agent("Rafi", ["Batya", "Aviva", "Gila"])
+    >>> s2 = Agent("Shlomo", ["Aviva", "Batya", "Gila"])
+    >>> s3 = Agent("Tomer", ["Batya", "Aviva", "Gila"])
+    >>> d1 = Agent("Gila", ["Shlomo", "Rafi", "Tomer"])
+    >>> d2 = Agent("Batya", ["Shlomo", "Rafi", "Tomer"])
+    >>> d3 = Agent("Aviva", ["Rafi", "Shlomo", "Tomer"])
+    >>> res = find_match([s1, s2, s3], [d1, d2, d3])
+    >>> res
+    {'Batya': 'Rafi', 'Aviva': 'Shlomo', 'Gila': 'Tomer'}
+    >>> res = find_match([d1, d2, d3], [s1, s2, s3])
+    >>> res
+    {'Rafi': 'Aviva', 'Shlomo': 'Batya', 'Tomer': 'Gila'}
     """
     matched_pairs = {}
     need_match = set(set1)
@@ -31,6 +43,7 @@ def find_match(set1: List[Agent], set2: List[Agent]) -> dict:
     while len(need_match) > 0:
         for dep in set2:
             interested_students = set()
+            interested_students.clear()
             cont = True
             for student in need_match:
                 if student.pref[0] == dep.name:
@@ -42,28 +55,26 @@ def find_match(set1: List[Agent], set2: List[Agent]) -> dict:
                     if not cont:
                         break
                     if student_name == student.name and matched_pairs.get(dep.name) is None:
-                        for s in interested_students:
-                            s.pref.pop(0)
                         matched_pairs.update({dep.name: student})
-                        # interested_students.remove(student)
-                        # temp_need_match.update(interested_students)
+                        need_match.remove(student)
                         for s in interested_students:
                             if not s == student:
                                 temp_need_match.add(s)
                         cont = False
                     elif student_name == student.name:
                         if get_index(dep.pref, student.name) < get_index(dep.pref, matched_pairs.get(dep.name).name):
-                            for s in interested_students:
-                                s.pref.pop(0)
                             old_s = matched_pairs.get(dep.name)
                             matched_pairs.update({dep.name: student})
-                            # interested_students.remove(student)
-                            # temp_need_match.update(interested_students)
+                            need_match.remove(student)
                             for s in interested_students:
                                 if not s == student:
                                     temp_need_match.add(s)
                             temp_need_match.add(old_s)
                             cont = False
+                        else:
+                            temp_need_match.add(student)
+        for s in need_match:
+            s.pref.pop(0)
         need_match.clear()
         need_match.update(temp_need_match)
         temp_need_match.clear()
@@ -82,20 +93,3 @@ def get_index(l , _val):
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-
-    # >>> s11 = Agent("Rafi", ["Batya", "Aviva", "Gila"])
-    # >>> s22 = Agent("Shlomo", ["Aviva", "Batya", "Gila"])
-    # >>> s33 = Agent("Tomer", ["Batya", "Aviva", "Gila"])
-    # >>> d11 = Agent("Gila", ["Shlomo", "Rafi", "Tomer"])
-    # >>> d22 = Agent("Batya", ["Shlomo", "Rafi", "Tomer"])
-    # >>> d33 = Agent("Aviva", ["Rafi", "Shlomo", "Tomer"])
-    # >>> res2 = find_match([s11, s22, s33], [d11, d22, d33])
-    # >>> print(res2)
-# s11 = Agent("Rafi", ["Batya", "Aviva", "Gila"])
-# s22 = Agent("Shlomo", ["Aviva", "Batya", "Gila"])
-# s33 = Agent("Tomer", ["Batya", "Aviva", "Gila"])
-# d11 = Agent("Gila", ["Shlomo", "Rafi", "Tomer"])
-# d22 = Agent("Batya", ["Shlomo", "Rafi", "Tomer"])
-# d33 = Agent("Aviva", ["Rafi", "Shlomo", "Tomer"])
-# res2 = find_match([s11, s22, s33], [d11, d22, d33])
-# print(res2)
